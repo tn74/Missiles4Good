e0909c656f974c959abf786b7d4aff561c7b6227
Trishul Nagenalli <tn74@duke.edu>
2018-11-30 18:52:41 -0500

    Cant get reliable data and it isn't printing some characters

--- lab7_skeleton_restored/input_brain.v
+++ lab7_skeleton_restored/input_brain.v
@@ -20,26 +20,35 @@ output[7:0] character_input;
 output reg start_writing_character;
 
 wire[7:0] ascii_char;
-reg[7:0] row_reg, col_reg;
-reg delayed_ps2_key_pressed;
+wire do_read_key;
 
 ps2_to_ascii converter(ps2_out, character_input);
 
+
 initial
 begin
-	row_reg <= 8'h00;
-	col_reg <= 8'h00;
 	start_writing_character <= 1'b0;
 end
 
-always @(posedge clock)
-begin
-	delayed_ps2_key_pressed <= ps2_key_pressed;
-end
+
+parameter TARGET_DELAY = 5000000; // 0.01 seconds
+reg[31:0] read_delay_counter;
+assign do_read_key = (read_delay_counter == TARGET_DELAY + 1);
 
 always @(posedge clock)
 begin
-	if (delayed_ps2_key_pressed && !(ascii_char == 8'h00)) 
+	if (!(read_delay_counter == TARGET_DELAY + 2))
+		read_delay_counter <= read_delay_counter + 1;
+	else if (ps2_key_pressed == 1'b1)
+		read_delay_counter <= 32'd1;
+end
+
+
+
+
+always @(posedge clock)
+begin
+	if (ps2_key_pressed && !(ascii_char == 8'h00)) 
 	begin
 		if (ascii_char == 8'h0A) // Ascii New Line
 		begin
@@ -50,11 +59,20 @@ begin
 		else // Else Write to terminal
 		begin
 			start_writing_character <= 1'b1;
-			if (row_reg < 8'h20)
+//			col_num <= col_num + 1;
+			if (col_num < 8'h20)
+			begin
 				col_num <= col_num + 1;
+			end
+			
 			else
+			begin
+			if (row_num < 8'h06)
 				row_num <= row_num + 1;
-				col_num <= 8'h00;
+			else
+				row_num <= 8'h00;
+			col_num <= 8'h00;
+			end
 		end
 	end
 	

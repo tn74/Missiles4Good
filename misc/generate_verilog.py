fe5a9d3b60fe2a417fe432e4cf9fcf941ace058e
Trishul Nagenalli <tn74@duke.edu>
2018-12-04 12:46:41 -0500

    Content from ready

--- misc/generate_verilog.py
+++ misc/generate_verilog.py
@@ -1,7 +1,7 @@
 def generate_ps2_line_updater():
 	lines = []
 	for i in range(32):
-		lines.append("\tps2_lines[7][{}] <= ps2_line_ready[{} : {}];\n".format(i, 255 - i * 8, 255 - i * 8 - 7))
+		lines.append("\tps2_lines[7][{}] <= ps2_line_content[{} : {}];\n".format(i, 255 - i * 8, 255 - i * 8 - 7))
 	
 	with open("line_ready.txt", "w") as f:
 		for l in lines:

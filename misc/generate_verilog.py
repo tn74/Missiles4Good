fc326e13b7cafbd152eb625fd7cb88d8d00848e7
Trishul Nagenalli <tn74@duke.edu>
2018-12-04 17:53:35 -0500

    ps2processor python lines are up

--- misc/generate_verilog.py
+++ misc/generate_verilog.py
@@ -2,20 +2,14 @@ def generate_ps2_line_updater():
 	lines = []
 	for i in range(32):
 		lines.append("\tps2_lines[7][{}] <= ps2_line_content[{} : {}];\n".format(i, 255 - i * 8, 255 - i * 8 - 7))
-	
-	with open("line_ready.txt", "w") as f:
-		for l in lines:
-			f.write(l)
+	save_to_file(lines, "line_ready.txt")
 
 def generate_ps2_lines_initiator():
 	lines = []
 	for i in range(8):
 		for j in range(32):
 			lines.append("\tps2_lines[{}][{}] <= 8'h00;\n".format(i, j))
-	
-	with open("lines_init.txt", "w") as f:
-		for l in lines:
-			f.write(l) 
+	save_to_file(lines, "lines_init.txt")
 
 def genereat_ps2_line_scroll():
 	lines = []
@@ -23,10 +17,24 @@ def genereat_ps2_line_scroll():
 		for col in range(32):
 			lines.append("ps2_lines[{}][{}] <= ps2_lines[{}][{}];\n".format(row - 1, col, row, col))
 
-	with open("lines_scroll.txt", "w") as f:
+	save_to_file(lines, "lines_scroll.txt")
+
+def generate_ps2processormodule_write_char():
+	lines = []
+	for i in range(0, 32):
+		lines.append("end else if (count == 32'd{}) begin\n".format(i))
+		lines.append("\tps2_line_content[{}:{}] <= ascii_char;\n".format(255 - i * 8, 255 - i * 8 - 7))
+	lines.append("end\n")
+	lines[0] = lines[0][9:]
+	save_to_file(lines, "ps2processormodule.txt")
+
+
+def save_to_file(lines, filename):
+	with open(filename, "w") as f:
 		for l in lines:
 			f.write(l) 
 
 generate_ps2_line_updater()
 generate_ps2_lines_initiator()
 genereat_ps2_line_scroll()
+generate_ps2processormodule_write_char()

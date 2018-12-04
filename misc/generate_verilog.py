e76dd309a32f29f9caaecfcab9577e96a504c3e2
Trishul Nagenalli <tn74@duke.edu>
2018-12-04 12:40:53 -0500

    Made the lines for updating bottom ps2 line

---
+++ misc/generate_verilog.py
@@ -0,0 +1,10 @@
+def generate_ps2_line_updater():
+	lines = []
+	for i in range(32):
+		lines.append("\tps2_lines[7][{}] <= ps2_line_ready[{} : {}];\n".format(i, 255 - i * 8, 255 - i * 8 - 7))
+	
+	with open("line_ready.txt", "w") as f:
+		for l in lines:
+			f.write(l)
+
+generate_ps2_line_updater()
